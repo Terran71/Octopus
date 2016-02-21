c6bce1a5fce6c3fc28d04718cfd58372401bd90c
Emily <emily@Emilys-MacBook-Pro.local>
2016-02-20 19:00:14 -0800

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -23,9 +23,8 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port    3000
 
-# port        ENV['PORT']     || 3000
+port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
